455b81268da87416786a6baf530c553a1a4f11e1
Kiztbz <pragyapravita@gmail.com>
2024-06-12 23:01:06 +0530

    EVENT-Listener

--- main.js
+++ main.js
@@ -10,5 +10,33 @@ player.sayMyName();
 player.draw(ctx);
 
 cvs.addEventListener("keyup", (e) => {
-    
+    switch (e.key) {
+        case "ArrowUp":
+            console.log("Up key released");
+            // Move player up
+            player.moveUp();
+            break;
+        case "ArrowDown":
+            console.log("Down key released");
+            // Move player down
+            player.moveDown();
+            break;
+        case "ArrowLeft":
+            console.log("Left key released");
+            // Move player left
+            player.moveLeft();
+            break;
+        case "ArrowRight":
+            console.log("Right key released");
+            // Move player right
+            player.moveRight();
+            break;
+        default:
+            console.log(`Key released: ${e.key}`);
+            break;
+    }
+
+    // Redraw the player after movement
+    ctx.clearRect(0, 0, cvs.width, cvs.height); // Clear the canvas
+    player.draw(ctx);
 })
